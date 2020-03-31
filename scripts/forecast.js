9ac232326238e84e7a761ecbf9e51526dc1fb484
MohdShoeb13 <62936389+MohdShoeb13@users.noreply.github.com>
2020-03-31 16:15:59 +0530

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,42 @@
+class Forecast {
+    constructor(){
+        this.key = '9GY7jdTpNuUAYvMJVfINgisrMZfaK6oK';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+
+    async updateCity(city){
+
+     const cityDets = await this.getCity(city);
+     const weather = await this.getWeather(cityDets.Key);
+     return { cityDets,weather};
+
+    }
+
+    async getCity(city){
+      
+        const query = `?apikey=${this.key}&q=${city}`;
+
+        const resposne = await fetch(this.cityURI + query);
+
+        const data = await resposne.json();
+
+        return data[0];
+
+    }
+
+    async getWeather(id){
+
+        const query = `${id}?apikey=${this.key}`;
+
+        const response = await fetch(this.weatherURI + query);
+
+        const data = await response.json();
+
+        return data[0];
+    }
+}
+
+  
+  
+
